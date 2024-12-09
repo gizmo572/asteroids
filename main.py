ab9d6c257201333d035e4aec407cfc4b8264c5bc
Nick Cushman <cushmann572@gmail.com>
2024-12-08 18:16:13 -0800

    init Asteroids.  render pygame window.

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    print(
+        f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}"
+    )
+
+
+if __name__ == "__main__":
+    main()
