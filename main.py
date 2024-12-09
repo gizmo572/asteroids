b78be3ca2c23fc15d7cac5b91204b87043798493
Nick Cushman <cushmann572@gmail.com>
2024-12-08 18:46:56 -0800

    feat: create circleshape class and player subclass. render player on screen as triangle.

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-    print(
-        f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}"
-    )
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
