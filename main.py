9e78afa9a694a33bb913e25c871a173bffdf2a65
Nick Cushman <cushmann572@gmail.com>
2024-12-09 13:07:01 -0800

    feat: add player to groups, update and draw all objects in group rather than just the player object explicitly.

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -16,9 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for sprite in updatable:
+            sprite.update(dt)
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
